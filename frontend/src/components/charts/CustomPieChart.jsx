7f838cfb2dce1e51c1788730b02426b61508e86d
Dawniqueca Steele <dawniquecas@gmail.com>
2026-09-12 23:53:24 -0400

    finance overview

---
+++ frontend/src/components/charts/CustomPieChart.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend} from "recharts";
+
+const CustomPieChart = ({
+    data,
+    label,
+    totalAmount,
+    colors,
+    showTextAnchor,
+}) => {
+    return (
+    <div>
+        
+    </div>    
+    )
+}
+
+export default CustomPieChart
--- frontend/src/components/dashboard/FinanceOverview.jsx
+++ frontend/src/components/dashboard/FinanceOverview.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import CustomPieChart from "../charts/CustomPieChart";
+
+const colors = ["#002626", "#24bdad", "#a4fbf7"]
 
 const FinanceOverview = ({ totalBalance, totalIncome, totalExpense }) => {
-    return(
-        <div>Finance Overview</div>
+    const balanceData = [
+        { name: "Total Balance", amount: totalBalance },
+        { name: "Total Expenses", amount: totalExpense },
+        { name: "Total Income", amount: totalIncome }
+    ];
+
+    return (
+        <div className="bg-white p-6 rounded-2xl shadow-md shadow-gray-100 border border-gray-200/50"> 
+        <div className="flex items-center justify-between">
+            <h5 className="text-lg">Financial Overview</h5>
+        </div>
+
+        <CustomPieChart
+        data={balanceData}
+        label="Total Balance"
+        totalAmount={`$${totalBalance}`}
+        colors={colors}
+        showTextAnchor
+        />
+        </div>
     )
 }
 
